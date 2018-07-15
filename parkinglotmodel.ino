295c51b8af147bb502fa660da69a57a4a6fe12f9
Mickey Dang <mickeydang7299@gmail.com>
2018-07-15 00:02:35 -0400

    linked code together

--- parkinglotmodel.ino
+++ parkinglotmodel.ino
@@ -1,18 +1,24 @@
-volatile int analogReader=0; //this is the variable that will be changed if an interrupt is detected
+//hardware variables
+volatile int analogReader = 0; //this is the variable that will be changed if an interrupt is detected
 int interruptPin = 2;
 int analogPin = A0;
+
+//constants
 const int MILLIS_PER_CLK = 4;
 const float MILLIS_IN_SECOND = (float) 1000;
 const int SCALE_FACTOR = 1000;
 const long CLK_PER_OVF = 65535;
+const int DELAY_TIME = 250;
 
+//dashboard and onboaring controls
 int stepNumber = 1;
-bool isIn=0;
-int parkingEntryDigits = 3;
-bool parkingNumber[parkingEntryDigits] = {0,0,0}
+bool isIn = 0;
+const int PARKING_ENTRY_DIGITS = 3;
+bool parkingNumber[PARKING_ENTRY_DIGITS] = {0,0,0};
 bool inputValid = false;
 
 
+//defined classes and structures
 class Time {
    public : volatile long overflowCount;
    public : volatile long registerCount = 0;
@@ -25,6 +31,7 @@ class Time {
 
 struct Spot {
   int index;
+  int pinId;
   bool isOccupied;
   Time startTime;
 };
@@ -51,29 +58,31 @@ class LinkedList {
   }
 
   void deleteHead() {
-      
      head = head->nextNode;
      delete head->prevNode;
-        
   }
   
 };
 
 
+//global variables
 LinkedList blinkQueue;
 
+//state variables
 volatile long overflow_count;
 volatile bool blinkLED;
+volatile int pinNumber;
 
-
+//parking lot model
 const int SIZE = 7;
 Spot spots[SIZE];
 
-
 void setup() {
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(interruptPin),pauseCount,RISING); //Interrupt 0 is mapped to pin 2, signal an interrupt on a change to pin 2
+  attachInterrupt(digitalPinToInterrupt(interruptPin), onButtonClicked, RISING); //Interrupt 0 is mapped to pin 2, signal an interrupt on a change to pin 2
 
+  //enable all pins we will be writing to
+  
   cli();
 
   blinkQueue = LinkedList();
@@ -84,8 +93,16 @@ void setup() {
       spots[i].startTime = Time();
   }
 
-  //enable overflow, compareA, compareB
+  //connect each spot to their arbitrary led pin id
+  spots[0].pinId = -1;
+  spots[1].pinId = -1;
+  spots[2].pinId = -1;
+  spots[3].pinId = -1;
+  spots[4].pinId = -1;
+  spots[5].pinId = -1;
+  spots[6].pinId = -1;
 
+  //enable overflow, compareA, compareB
   TIMSK1 = 0x7;
 
   TCCR1A &= ~(1 << 0);
@@ -94,8 +111,7 @@ void setup() {
   TCCR1B &= ~(1 << 2);
   TCCR1B |= (1 << 1);
   TCCR1B |= (1 << 0);
-  
-  OCR1B = 50000;
+ 
   OCR1A = 40000;
   
   TCNT1 = 0;
@@ -105,22 +121,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly
-  analogReader = analogRead(analogPin); // reads analogPin (A0)
-  //POLL FOR TIMERS?
   
+  if (blinkLED && pinNumber != -1) {
+     blink(pinNumber);
+     resetReadyToBlink();
+  }
 
-   if (blinkLED) {
-      //find the LED Pin
-      //set to high
-      //delay
-      //set to low
-      //change linkedlist
-   }
-   
-   float seconds = (double) getTimeMillis() / (float) 1000;
-   Serial.println("seconds since start: " + String(seconds));
-   delay(1000);
-
+   //debug
 }
 
 ISR (TIMER1_OVF_vect) {
@@ -128,18 +135,22 @@ ISR (TIMER1_OVF_vect) {
 }
 
 ISR (TIMER1_COMPA_vect) {
+
+  if (overflow_count >= blinkQueue.head->timeToFire.overflowCount) {
+    int i = blinkQueue.head->index;
+    Time nextTime = convertIndexToTime(i);
+    
+    Node* n = new Node();
+    n->timeToFire = nextTime;
+    n->index = i;
+    
+    blinkQueue.modifyList(n, blinkQueue.head);
+    blinkQueue.deleteHead();
   
-  int i = blinkQueue.head->index;
-  blink(i);
-  Time nextTime = convertIndexToTime(i);
-  
-  Node* n = new Node();
-  n->timeToFire = nextTime;
-  n->index = i;
-  
-  blinkQueue.modifyList(n, blinkQueue.head);
-  blinkQueue.deleteHead();
-  
+    OCR1A = blinkQueue.head->timeToFire.registerCount;
+    setReadyToBlink(spots[i].pinId);
+  }
+     
 }
 
 ISR (TIMER1_COMPB_vect) {
@@ -151,8 +162,21 @@ Time convertIndexToTime(int i) {
   return aTime;
 }
 
-void blink (int id) {
-  
+void blink (int pinId) {
+   digitalWrite(pinId, HIGH);
+   delay(DELAY_TIME);
+   digitalWrite(pinId, LOW);
+   
+}
+
+void setReadyToBlink(int pinId) {
+    blinkLED = true;
+    pinNumber = pinId;
+}
+
+void resetReadyToBlink() {
+    blinkLED = false;
+    pinNumber = -1;
 }
 
 void onSpotSelected(int index, int rC) {
@@ -206,18 +230,11 @@ long differenceInTimeCLK(Time t1, Time t2) {
     return oc * CLK_PER_OVF + rc;
 }
 
-//program interrupt logic for overflow
-
-//program button handling
-
-
 int getParkingNumber(){
    return parkingNumber[0] + parkingNumber[1] * 2  + parkingNumber[2] * 4;
 }
 
-
-void pauseCount(){
-  Serial.println("Interrupt triggered");
+void onButtonClicked(){
 
   if(analogReader < 5 && analogReader > 0){// 1 - 4
    //weird...
@@ -226,34 +243,36 @@ void pauseCount(){
    //weird...
    
   }else if(analogReader < 10 && analogReader > 0){ // 9 - 1
-
+    //button press on first button
     parkingNumber[0] = !parkingNumber[0];
     stepNumber = 2;
     
   }else if(analogReader < 40 && analogReader > 32){ // 39-33
-
+    //button press on second button
     parkingNumber[1] = !parkingNumber[1];
     stepNumber = 2;
 
   }else if(analogReader < 51 && analogReader > 43){ // 50-44
-   
+    //button press on third button
     parkingNumber[2] = !parkingNumber[2];
     stepNumber = 2;
 
-  }else if(analogReader < 185 && analogReader > 175){ // 184-176 //IN IN IN IN 
+  }else if(analogReader < 185 && analogReader > 175){ // 184-176 
+    //in button press
     if(stepNumber == 2){
       isIn = true;
       stepNumber = 3;  
-      
     }
     
-  }else if(analogReader < 252 && analogReader > 240){ // 251-241  //OUT OUT OUT 
+  }else if(analogReader < 252 && analogReader > 240){ // 251-241  
+    //out button press
     if(stepNumber == 2){
       isIn = false;
       stepNumber = 3;  
     }
     
   }else if(analogReader < 860 && analogReader > 850){ // 859-851
+    //go button press
     if(stepNumber == 3){
       if(isIn){
         onSpotSelected(getParkingNumber(), TCNT1);  
@@ -263,9 +282,10 @@ void pauseCount(){
       //resets:
       stepNumber = 1;
     }
-  }else if(analogReader < 911 && analogReader > 899){ // 910-900
     
-  }else{ // 1024
+  }else if(analogReader < 911 && analogReader > 899){ // 910-900
+    //todo add cancel button 
+  }else { // 1024
     //normal do nothing
   }
   
